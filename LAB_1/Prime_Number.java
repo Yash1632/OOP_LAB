29e8e55e0f19767d0b85fbccc945bccc1dc2a5d6
Yash1632 <yash160203@gmail.com>
2021-01-15 12:14:44 +0530

    Java code to print prime_numbers upto 1000

---
+++ LAB_1/Prime_Number.java
@@ -0,0 +1,21 @@
+public class Prime_Number {
+    public static void main(String[] args){
+        for(int i=2; i<=1000; i++){
+            int j = 0;
+            //loop to check if number is prime
+            for(int k=2; k<i; k++){
+                /* if condition to check divisiblity
+                   from numbers between and number.
+                   If divisible, then it is not prime */
+                if(i%k==0){
+                    j++;
+                    break;
+                }
+            }
+            //j==0 for prime and not=0 for non-prime
+            if(j == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
