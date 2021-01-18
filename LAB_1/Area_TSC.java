Yash1632/OOP_LAB

commit ef9c67380e0f91573ae06ebd17d0d8229db50ea9

Updated Package(forlder which contail .java files) details

LAB_1/Area_TSC.java
@@ -1,3 +1,5 @@
+package LAB_1;
+
 public class Area_TSC {
     public static void main(String[] args){
         int l = Integer.parseInt(args[0]);

LAB_1/Matrix_Mult.java
@@ -1,4 +1,6 @@
-public class Matrix_Mult{
+package LAB_1;
+
+public class Matrix_Mult {
 
     public static void main(String[] args){
         if(args.length<1){

LAB_1/Matrix_Transpose.java
@@ -1,3 +1,5 @@
+package LAB_1;
+
 public class Matrix_Transpose {
     public static void main(String[] args){
         if(args.length<1){

LAB_1/Prime_Number.java
@@ -1,3 +1,5 @@
+package LAB_1;
+
 public class Prime_Number {
     public static void main(String[] args){
         for(int i=2; i<=1000; i++){

LAB_1/Print_Table.java
@@ -1,3 +1,5 @@
+package LAB_1;
+
 public class Print_Table
 {
     public static void main(String[] args){

commit 46c6d79700ea786f6655bcc9b6ae7377a9b6a6df

FIX ERRORS CAUSED BY COMMIT ef9c673

LAB_1/Area_TSC.java
@@ -1,5 +1,3 @@
-package LAB_1;
-
 public class Area_TSC {
     public static void main(String[] args){
         int l = Integer.parseInt(args[0]);

LAB_1/Matrix_Mult.java
@@ -1,5 +1,3 @@
-package LAB_1;
-
 public class Matrix_Mult {
 
     public static void main(String[] args){

LAB_1/Matrix_Transpose.java
@@ -1,5 +1,3 @@
-package LAB_1;
-
 public class Matrix_Transpose {
     public static void main(String[] args){
         if(args.length<1){

LAB_1/Prime_Number.java
@@ -1,5 +1,3 @@
-package LAB_1;
-
 public class Prime_Number {
     public static void main(String[] args){
         for(int i=2; i<=1000; i++){

LAB_1/Print_Table.java
@@ -1,5 +1,3 @@
-package LAB_1;
-
 public class Print_Table
 {
     public static void main(String[] args){